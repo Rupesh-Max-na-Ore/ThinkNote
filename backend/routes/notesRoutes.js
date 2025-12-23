62f8006bf41132e20f83f6d40ccd806d2047e511
Rupesh <dasrupesh2124@gmail.com>
2025-12-23 14:18:29 +0530

    Make separate controller functions file for api routes, clean code

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,22 +1,11 @@
 import express from "express"
-
+import { createNote, deleteNote, getAllNotes, updateNote } from "../controllers/notesController";
 const router = express.Router();
 
 // Endpoint codes (routes)
-router.get("/", (req, res) => {
-    res.status(200).send("You have just fetched the Notes!");
-});
-
-router.post("/", (req, res) => {
-    res.status(201).json({message:"Note created successfully!"})
-});
-
-router.put("/:id", (req, res) => {
-    res.status(200).json({message:"Note updated successfully!"})
-});
-
-router.delete("/:id", (req, res) => {
-    res.status(200).json({message:"Note deleted successfully!"})
-});
+router.get("/", getAllNotes);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 export default router;
