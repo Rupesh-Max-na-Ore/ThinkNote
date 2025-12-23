8b1380c502d7112294c0a2d5cd3824c08a90ef0b
Rupesh <dasrupesh2124@gmail.com>
2025-12-23 14:02:21 +0530

    Added Routers to separate file

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -2,4 +2,21 @@ import express from "express"
 
 const router = express.Router();
 
+// Endpoint codes (routes)
+router.get("/", (req, res) => {
+    res.status(200).send("You have just fetched the Notes!");
+});
+
+router.post("/", (req, res) => {
+    res.status(201).json({message:"Note created successfully!"})
+});
+
+router.put("/:id", (req, res) => {
+    res.status(200).json({message:"Note updated successfully!"})
+});
+
+router.delete("/:id", (req, res) => {
+    res.status(200).json({message:"Note deleted successfully!"})
+});
+
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js"
 
 const app = express();
 
-app.use("/api/notes", notesRoutes)
-
-// Endpoint codes (routes)
-app.get("/api/notes", (req, res) => {
-    res.status(200).send("you got many notes");
-});
-
-app.post("/api/notes", (req, res) => {
-    res.status(201).json({message:"Note created successfully!"})
-});
-
-app.put("/api/notes/:id", (req, res) => {
-    res.status(200).json({message:"Note updated successfully!"})
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-    res.status(200).json({message:"Note deleted successfully!"})
-});
+app.use("/api/notes", notesRoutes);
 
 app.listen(5001, () => {
     console.log("Server started on PORT: 5001");
