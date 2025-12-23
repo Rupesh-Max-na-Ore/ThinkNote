531145c5a0a9954f0405d90d5375c73c0bda5cf1
Rupesh <dasrupesh2124@gmail.com>
2025-12-24 02:19:12 +0530

    implement redis rate limiter using upstash

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-
+import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 // console.log(process.env.MONGO_URI); //test if can access .env
@@ -14,6 +14,7 @@ connectDB();
 
 // middleware -  parses incoming JSON request bodies and makes the data available on req.body
 app.use(express.json());
+app.use(rateLimiter);
 
 // // simple custom middleware
 // app.use((req, res, next) => {
