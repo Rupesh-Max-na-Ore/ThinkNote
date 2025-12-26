bfcacda808c6deb2417c35d4f693074aed1d9e83
Rupesh <dasrupesh2124@gmail.com>
2025-12-26 23:04:49 +0530

    NoteCard component prepared

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from 'cors';
+
 dotenv.config();
 
 // console.log(process.env.MONGO_URI); //test if can access .env
@@ -10,8 +12,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// app.use(cors()); //keep this if want to allow every site
+// for allowing only our specific frontend to access it
+app.use(cors({origin: "http://localhost:5173",}));
+
+
 // middleware -  parses incoming JSON request bodies and makes the data available on req.body
 app.use(express.json());
+
+
 app.use(rateLimiter);
 
 
